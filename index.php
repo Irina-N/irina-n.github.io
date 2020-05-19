3ab809295f8233b9d2876c19be2e783ad16b2560
Irina-N <62673713+Irina-N@users.noreply.github.com>
2020-05-19 23:36:52 +0300

    Puslapis

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf=8">
+	<link rel="stylesheet" href="style.css">
+	<title>Mano puslapis</title>
+</head>
+<body>
+
+<div class="content">
+	<div class="header">
+		<a href="index.php">Главная</a>
+		<a href="riddles.php">Загадки</a>
+		<a href="guessTheNumber.html">Угадай число</a>
+
+	</div>	
+	
+	<h1>Mano pusLAPĖ</h1>
+
+	<div class="center">
+	<img src="img/fox.jpg">
+		<div class="box_text">
+		<p>Этот сайт сделан для размещения результатов моих стараний в рамках обучения <i>основам программирования</i>.</p>
+		<p>Здесь должна быть ссылка на сайт <b>Geek Brains</b>, но вместо неё мы пройдём <a href="https://youtu.be/R4mT1-_enU0">сюда</a> и посмотрим на пёсиков.</p>
+		<p>Вот мои труды: 
+		<a href="index.html">Главная</a>
+		<a href="riddles.html">Загадки</a>
+		<a href="guessTheNumber.html">Угадай число</a>
+		<a href="typing.html">Слепая печать</a></p>
+		</div> 
+	</div>
+</div>
+<div class="footer">
+	Copyright &copy; <?php echo "date(Y)"; ?> I.N.
+</div>
+
+</body>
+</html>
